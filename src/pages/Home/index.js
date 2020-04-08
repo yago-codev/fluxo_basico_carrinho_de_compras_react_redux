0ffc2ec85ef94efc250999b44417e09a2231a4db
yago-codev <yagomilano92@gmail.com>
2020-04-08 18:16:04 -0300

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,112 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://imageswscdn.wslojas.com.br/files/20717/MED_nike-kyrie-5-patrick-175972.png"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://imageswscdn.wslojas.com.br/files/20717/MED_nike-kyrie-5-patrick-175972.png"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://imageswscdn.wslojas.com.br/files/20717/MED_nike-kyrie-5-patrick-175972.png"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://imageswscdn.wslojas.com.br/files/20717/MED_nike-kyrie-5-patrick-175972.png"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://imageswscdn.wslojas.com.br/files/20717/MED_nike-kyrie-5-patrick-175972.png"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://imageswscdn.wslojas.com.br/files/20717/MED_nike-kyrie-5-patrick-175972.png"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    background: linear-gradient(145deg, #404347, #070809);
+    /* background: #070809; */
+    border-radius: 30px;
+    box-shadow: 5px 5px 5px #2d2f32, -5px -5px 5px #4b4f53;
+    padding: 20px;
+
+    img {
+      max-width: 250px;
+      align-self: center;
+    }
+
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      background: #e9520d;
+      color: #fff;
+      border: 0;
+      border-radius: 20px;
+      box-shadow: inset 3px 3px 3px #af3e0a, inset -3px -3px 3px #ff6710;
+      overflow: hidden;
+      margin-top: auto;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, '#e9520d')};
+        box-shadow: inset 3px 3px 3px ${darken(0.05, '#af3e0a')},
+          inset -3px -3px 3px ${darken(0.05, '#ff6710')};
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 12px 18px;
+        /* background: #cf4a0c;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+        box-shadow: inset 3px 3px 3px #9b3809, inset -3px -3px 3px #ff5d0f; */
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        margin-right: 28px;
+        font-weight: bold;
+      }
+    }
+  }
+`;
