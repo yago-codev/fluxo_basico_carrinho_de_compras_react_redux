14fe96cfc14e4455a09796db221758494e2ef083
yago-codev <yagomilano92@gmail.com>
2020-04-10 03:28:15 -0300

    Criando o switch para a action de deleção de produtos e dispachando a action através do botão da página Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -8,7 +8,7 @@ import { connect } from 'react-redux';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -50,7 +50,12 @@ function Cart({ cart }) {
                 <strong>R$258,60</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} />
                 </button>
               </td>
